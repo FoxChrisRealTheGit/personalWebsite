7478c2e0fc9b82e5174a22a5b6a381dc6e628a9f
Christopher Fox <cfox1290@gmail.com>
2018-02-06 19:55:09 -0700

    initial imports

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+    Container1,
+} from "react-stylux";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <Container1>
+            </Container1>
+        )
+    }
+}
---
+++ src/router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+    <Switch>
+        <Route to="" component></Route>
+        <Route to="" component></Route>
+        <Route to="" component></Route>
+    </Switch>
+
+)
