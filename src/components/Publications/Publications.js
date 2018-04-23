2a6ec5533ef57ecec80255c56e9d838ad07793e0
Christopher Fox <cfox1290@gmail.com>
2018-04-23 14:03:55 -0600

    some publication adds

--- src/components/Publications/Publications.js
+++ src/components/Publications/Publications.js
@@ -6,7 +6,11 @@ import {
     Container,
     Holder,
     H31,
+    H42,
     HR,
+    A,
+    SquareImage,
+    BulletList1,
 } from 'react-stylux';
 
 export default class Publications extends Component {
@@ -16,13 +20,242 @@ export default class Publications extends Component {
             <section>
                 <Header />
                 <Container>
-                <HR height="300px" />
-                    <Holder>
+                    <HR height="300px" />
+                    <Holder
+                        direction="column">
                         <H31>
                             Publications
                         </H31>
-
-                        
+                        <HR />
+                        <H42>Fiction</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for Alone"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Alone
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                            <HR />
+                        </Holder>
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for Gendered"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Gendered
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
+                    </Holder>
+                    <Holder direction="column">
+                        <HR />
+                        <H42>Non-Fiction</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for I Wanted to Die"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                I Wanted to Die
+                                        </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                            <HR />
+                        </Holder>
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for I Wanted to Die"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                CryptoCurrency: A History
+                                        </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
+                    </Holder>
+                    <Holder direction="column">
+                        <HR />
+                        <H42>Short Stories</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for Under The Apple Tree"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Under the Apple Tree
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
+                    </Holder>
+                    <Holder direction="column">
+                        <HR />
+                        <H42>Poetry</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for Poetry Collection I"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Poetry Collection I
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
+                    </Holder>
+                    <Holder direction="column">
+                        <HR />
+                        <H42>Plays</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"The cover for Melanine and Joanne"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Melanine and Joanne
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
+                    </Holder>
+                    <Holder direction="column">
+                        <HR />
+                        <H42>Misc Scripts</H42>
+                        <HR />
+                        <Holder
+                            direction="column">
+                            <Holder
+                                direction="column">
+                                <A width="85%" to="">
+                                    <Holder
+                                        alignBlock1="flex-start">
+                                        <SquareImage
+                                            size='lg'>
+                                            {}
+                                            {"An Image from Dad and Other Dad"}
+                                        </SquareImage>
+                                        <Holder
+                                            direction="column">
+                                            <H42 margin="0 0 50px 0">
+                                                Dad and Other Dad
+                                            </H42>
+                                            <BulletList1>
+                                                {"Just starting"}
+                                            </BulletList1>
+                                        </Holder>
+                                    </Holder>
+                                </A>
+                            </Holder>
+                        </Holder>
                     </Holder>
                     <HR height="300px" />
                 </Container>
