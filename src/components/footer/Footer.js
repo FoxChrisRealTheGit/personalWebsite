aa561b6898e62d65e72b62123b948079ffbd790c
Christopher Fox <cfox1290@gmail.com>
2018-05-26 12:47:56 -0600

    fixed footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -17,13 +17,14 @@ export default function Footer() {
                     Connect With Me!
                     </H>
                 <Navbar
-                    navbarWidth='60%' >
+                    box="1"
+                    totalWidth='60%' >
                     <a href="https://facebook.com/FoxDesignAgency/" target="_blank" rel="noopener noreferrer"></a>
                     <a href="https://twitter.com/foxchrisrealthe" target="_blank" rel="noopener noreferrer"></a>
                     <a href="https://github.com/FoxChrisRealTheGit" target="_blank" rel="noopener noreferrer"></a>
                     <a href="https://linkedin.com/in/foxchrisrealthe" target="_blank" rel="noopener noreferrer"></a>
                 </Navbar>
-                <P box="2">Copyright &copy; 2018 - All Rights Reserved FoxChrisRealThe- Website Design by: <a href="https://FoxDesignAgency.com" target="_blank" rel="noopener noreferrer">Fox Design Agency</a></P>
+                <P box="2">Copyright &copy; 2018 - All Rights Reserved FoxChrisRealThe - Website Design by: &nbsp;<a href="https://FoxDesignAgency.com" target="_blank" rel="noopener noreferrer">Fox Design Agency</a></P>
             </Holder>
         </Wrapper>
     )
